91a27942fbede5607bd21080c829e0e51c8f2e11
Eduardo Bordin <dudubordin26@gmail.com>
2022-04-29 01:43:27 -0300

    Update/3

--- scripts_/stopwatch.js
+++ scripts_/stopwatch.js
@@ -1,55 +1,92 @@
 const btnStart = document.getElementById('start');
 const btnPause = document.getElementById('pause');
 const btnStop = document.getElementById('stop');
+const btnSteps = document.getElementById('steps');
+const btnDeleteAll = document.getElementById('deleteAll');
 const counter = document.getElementById('counter');
+const divCounter = document.querySelector('.divStep');
+const pTest = document.querySelector('.pTest');
 
-let cron = Number();
 let timer;
 let speed = 100;
 
-function getTime(seconds){
+let hh = 0;
+let mm = 0;
+let ss = 0;
 
-    let getNewTime = new Date(seconds * 1000);
-    return getNewTime.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
-    })
+let clickTet = 0;
+
+btnPause.disabled = true;
+
+btnDeleteAll.onclick = () =>{
+
+    document.location.reload();
+};
+
+function test(){
+
+    ss++;
+
+    if(ss > 60){
+        ss = 0;
+        mm++;
+
+        if(mm > 60){
+
+            mm = 0;
+            hh++;
+        }
+    }
+
+    let format = (hh < 10 ? '0' + hh : hh) + `:` + (mm < 10 ? '0' + mm : mm) + `:` + (ss < 10 ? '0' + ss : ss);
+
+    counter.innerHTML = format;
 }
 
 function createTime(){
 
-    timer = setInterval(function(){
-        cron++;
-        counter.innerHTML = getTime(cron);
-        
-        if(cron >= 10){
-           
-            let addP1 = document.createElement('p');
-            addP1.innerHTML = "Deu certo";
-            addP1.id = "pTest";
-
-            counter.appendChild(addP1);
-
-            if(cron >= 100  ){
-
-                let addP2 = document.createElement('p');
-            addP2.innerHTML = "Deu certooo";
-            addP2.id = "pTest";
-            
-            addP1.remove();
-            counter.appendChild(addP2);
-            }
-
-        }
-
-    }, speed)
+    timer = setInterval( test , speed);       
 }
 
 
+btnSteps.addEventListener('click', function(){
+    
+    clickTet++;
+
+    console.log(clickTet);
+
+    if(clickTet >= 5){
+        btnSteps.disabled = true;
+        clickTet = 5;
+    }
+
+    let addP1 = document.createElement('p');
+    addP1.innerHTML = `${hh} : ${mm} : ${ss}`;
+    addP1.id = "pTest";
+
+    divCounter.appendChild(addP1);
+
+    let btn = document.createElement('button');
+    btn.id = "btnTest";
+    btn.innerHTML = `<i class="fa-solid fa-delete-left" id="delBtn"></i>`;
+
+    divCounter.appendChild(btn);
+
+    btn.onclick = () => {
+        addP1.remove();
+        btn.remove();
+        btnSteps.disabled = false;
+        clickTet-= 1;
+    };
+
+});
+
 btnStart.addEventListener('click', function(){
 
+    
     createTime();
     btnStart.disabled = true;
+    btnPause.disabled = false;
     btnStart.style.backgroundColor = "rgb(203, 107, 107)";
 
     counter.style.color = "white";
@@ -60,9 +97,9 @@ btnPause.addEventListener('click', function(){
 
     clearInterval(timer);
     btnStart.disabled = false;
+    btnSteps.disabled = true;
     btnStart.style.backgroundColor = "rgb(255, 71, 71)";
-
-    counter.style.color = "red";
+    counter.style.color = "rgb(255, 71, 71)";
     counter.style.transition = "0.5s";
     counter.style.transform = "Scale(1.2)";
 });
@@ -71,10 +108,13 @@ btnStop.addEventListener('click', function(){
 
     clearInterval(timer);
     counter.innerHTML = "00:00:00";
-    cron = 0;
     btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnSteps.disabled = true;
     btnStart.style.backgroundColor = "rgb(255, 71, 71)";
-
     counter.style.color = "white";
     counter.style.transform = "Scale(1)";
+    hh = 0;
+    mm = 0;
+    ss = 0;
 });
